8f109e778cadaea79439bf54d2d4cecde30c9fe3
Daria Voo <dasha2003.voo@yandex.ru>
2019-10-20 15:28:01 +0300

    Работает с картой!)))

--- app/src/main/java/com/example/myrecycleview/MainActivity.kt
+++ app/src/main/java/com/example/myrecycleview/MainActivity.kt
@@ -1,27 +1,49 @@
 package com.example.myrecycleview
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 //import android.support.constraint.ConstraintLayout
 
-import android.util.Log
+//import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+//Для карты
+import android.location.Location
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.bottom_sheet.*
 import kotlinx.android.synthetic.main.main_content.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener,
+    //Интерфейсы для карт
+    OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+    //Для футбара
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
+    //Для карты
+    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE  = 1
+    }
+    //Для данных
     private lateinit var linearLayoutManager: LinearLayoutManager
-
-    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>//для футбара
-
     private val persons = arrayListOf<Person>()
     private fun initializeData() {
 
@@ -50,13 +72,80 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             layoutManager = linearLayoutManager
             adapter = MyRecyclerAdapter(persons)
         }
-        //вызов функции,которая собирает футор - в разработке
+        //Вызов функции,которая собирает футор - в разработке
         initBottomSheet()
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        //Получение запроса о последнем известном местоположении
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+    }
+    //Работа с картами
+    override fun onMarkerClick(p0: Marker?) : Boolean {
+        Toast.makeText(this, "yea", Toast.LENGTH_SHORT).show()
+        slideUpDownBottomSheet()
+        return false
+    }
+    private fun setUpMap() {
+        //Проверка, есть ли у нас права на доступ к местоположению пользователя
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) {
+            //Если нет, то запрашиваем разрешение
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+            return
+        }
+    }
+    private fun placeMarkerOnMap(location: LatLng){
+        //Создание объекта MarkerOptions и установка текущего
+        //местоположения как позиция для маркера
+        val markerOptions = MarkerOptions().position(location)
+        //Добавление маркера на карту
+        mMap.addMarker(markerOptions)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        //Добавление маркера остановки МИЭТ
+        val miet = LatLng(55.983174, 37.210626)
+        mMap.addMarker(MarkerOptions().position(miet).title("Остановка МИЭТ"))
+        //Наведение камеры на маркер и приближение
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12.0f))
+
+        //Подключение элементов управления масштабом на карте
+        mMap.uiSettings.isZoomControlsEnabled = true
+        //Подключение листенера при клике на маркер на карте
+        mMap.setOnMarkerClickListener(this)
+        setUpMap()
+
+        //добавляет синюю точку на user’s location
+        //и кнопку на карте,при нажатии на которую карта центрируется относительно пользователя
+        mMap.isMyLocationEnabled = true
+
+        //Даёт самое последнее местоположение,доступное в текущее время
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            // Got last known location. In some rare situations this can be null.
+            // Если удалось получить самое последнее местоположение
+            if (location != null) {
+                //переместить камеру в текущее местоположеие пользователя
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
     }
 
     private fun initBottomSheet() {
-        //Устанавливаем листенер (обработчик событий) на кнопку
-        buttonMain.setOnClickListener(this)
+        //Устанавливаем листенер (обработчик событий) на кнопку Не надо
+        //buttonMain.setOnClickListener(this)
 
         //подключаем дейстия на BottomSheet
         bottomSheetBehavior = BottomSheetBehavior.from<LinearLayout>(bottom_sheet)
@@ -71,15 +160,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 when (newState) {
                     BottomSheetBehavior.STATE_COLLAPSED -> {
-                        buttonMain.visibility = View.VISIBLE
+                        //buttonMain.visibility = View.VISIBLE
                         //buttonMain.text = "Slide Up"
                     }
                     BottomSheetBehavior.STATE_HIDDEN -> {
-                        buttonMain.visibility = View.VISIBLE
+                        //buttonMain.visibility = View.VISIBLE
                         //buttonMain.text = "Slide Down"
                     }
                     BottomSheetBehavior.STATE_EXPANDED -> {
-                        buttonMain.visibility = View.INVISIBLE
+                        //buttonMain.visibility = View.INVISIBLE
                         // buttonMain.text = "Slide Down"
                     }
                     BottomSheetBehavior.STATE_DRAGGING -> {
@@ -92,30 +181,27 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
         })
     }
-
     //Листенер для того, на что можно кликать
     override fun onClick(clickView: View?) {
         when (clickView) {//Если нажали на какую-то кнопку
-            buttonMain -> {//нажали на кнопку с id button
+            /*buttonMain -> {//нажали на кнопку с id button
                 slideUpDownBottomSheet()
-            }
+            }*/
         }
     }
-
     private fun slideUpDownBottomSheet() {
         //Проверяем состояние BottomSheet
         if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_HIDDEN) {
             //Если BottomSheet не открыт, открываем его
             bottomSheetBehavior.peekHeight = 600
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-            buttonMain.text = "Slide Down"
+            //buttonMain.text = "Slide Down"
         } else {
             //Сворачиваем BottomSheet
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
-            buttonMain.text = "Slide Up"
+            //buttonMain.text = "Slide Up"
         }
     }
-
     //Подключение меню
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
@@ -137,5 +223,4 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
         return super.onOptionsItemSelected(item)
     }
-
 }
